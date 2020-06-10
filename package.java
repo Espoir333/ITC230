61982962bf893dbe1d6c9c127ae5725d07fa7698
Espoir Makiese <44072742+Espoir333@users.noreply.github.com>
2020-06-10 12:07:33 -0700

    Create package.java

---
+++ package.java
@@ -0,0 +1,20 @@
+{
+  "name": "itc230",
+  "version": "1.0.0",
+  "description": "web-server APP",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [
+    "Espoir",
+    "Lewis",
+    "itc230",
+    "spring",
+    "2020",
+    "web-server",
+    "APP"
+  ],
+  "author": "Espoir Makiese",
+  "license": "ISC"
+}
